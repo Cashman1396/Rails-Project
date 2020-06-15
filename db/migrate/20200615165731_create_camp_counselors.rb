063bcd085fd11ef52442438169dea7d756540ff6
chris_ashman <chrisashman96@gmail.com>
2020-06-15 13:00:33 -0400

    added migration files to database

---
+++ db/migrate/20200615165731_create_camp_counselors.rb
@@ -0,0 +1,6 @@
+class CreateCampCounselors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :camp_counselors do |t|
+    end
+  end
+end
---
+++ db/migrate/20200615165801_create_assignments.rb
@@ -0,0 +1,6 @@
+class CreateAssignments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :assignments do |t|
+    end
+  end
+end
