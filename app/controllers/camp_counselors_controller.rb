e1c9130e6b752e5e0eaf7feb5dedd297699af2ab
chris_ashman <chrisashman96@gmail.com>
2020-06-21 02:40:59 -0400

    added controller files and root in config

---
+++ app/controllers/camp_counselors_controller.rb
@@ -0,0 +1,27 @@
+class CampCounselorsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy 
+  end
+
+  private
+
+  def counselors_params
+  end
+end
