289b4c7d1cb97a9eae583050a397fe6e55d4241c
chris_ashman <chrisashman96@gmail.com>
2020-06-22 23:40:41 -0400

    added code to camp counselor

--- app/controllers/camp_counselors_controller.rb
+++ app/controllers/camp_counselors_controller.rb
@@ -1,14 +1,24 @@
 class CampCounselorsController < ApplicationController
-  def index
-  end
+  skip_before_action :require_login, only: [:new, :create]
+  before_action :set_counselor, only: [:show, :edit, :update, :destroy]
+  include CampCounselorsHelper
 
   def show
+    @counselor = Counselor.find(params[:id])
   end
 
   def new
+    @counselor = CampCounselor.new
   end
 
   def create
+    @counselor = CampCounselor.create(counselors_params)
+    if @counselors.save 
+      session[:user_ud] = @counselor.id
+      redirect_to camp_counselor_path(@counselor)
+    else 
+      render :new
+    end
   end
 
   def edit
