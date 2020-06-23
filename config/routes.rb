d9a3e811c186c54313f75d35b2b6cc7d421a3cd9
chris_ashman <chrisashman96@gmail.com>
2020-06-23 15:53:36 -0400

    added partials to views and code to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sessions#new'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get  '/logout' => 'sessions#destroy'
+
+  resources :camp_counselors do
+    resources :assignments, only: [:show, :index]
+    resources :comments, shallow: true
+  end 
 end
